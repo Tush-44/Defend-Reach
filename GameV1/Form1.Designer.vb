2840f6faf23e81ea66a3a193e6a1dfa57ff8dd38
Tush-44 <75323605+Tush-44@users.noreply.github.com>
2020-12-03 08:25:22 -0700

    Add files via upload

---
+++ GameV1/Form1.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.ArizonWall = New System.Windows.Forms.PictureBox()
+        Me.Bullet = New System.Windows.Forms.PictureBox()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ArizonWall, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Bullet, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.Image = Global.GameV1.My.Resources.Resources._811k0lk_ykL__AC_UL1500_
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 151)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(195, 229)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox3.Image = Global.GameV1.My.Resources.Resources.covenant_elite_minor_mega_construx_clash_on_ring_halo_800x1
+        Me.PictureBox3.Location = New System.Drawing.Point(594, 139)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(194, 229)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 2
+        Me.PictureBox3.TabStop = False
+        '
+        'ArizonWall
+        '
+        Me.ArizonWall.Image = Global.GameV1.My.Resources.Resources._5_newmethodfor
+        Me.ArizonWall.Location = New System.Drawing.Point(-3, -5)
+        Me.ArizonWall.Name = "ArizonWall"
+        Me.ArizonWall.Size = New System.Drawing.Size(1679, 45)
+        Me.ArizonWall.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.ArizonWall.TabIndex = 3
+        Me.ArizonWall.TabStop = False
+        '
+        'Bullet
+        '
+        Me.Bullet.BackColor = System.Drawing.Color.Transparent
+        Me.Bullet.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Bullet.Image = Global.GameV1.My.Resources.Resources.download
+        Me.Bullet.Location = New System.Drawing.Point(213, 151)
+        Me.Bullet.Name = "Bullet"
+        Me.Bullet.Size = New System.Drawing.Size(27, 27)
+        Me.Bullet.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Bullet.TabIndex = 4
+        Me.Bullet.TabStop = False
+        Me.Bullet.UseWaitCursor = True
+        Me.Bullet.Visible = False
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(61, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "ScoreLabel"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.GameV1.My.Resources.Resources._8381171693f7fedc6b6411c39f15f9fb
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Bullet)
+        Me.Controls.Add(Me.ArizonWall)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.DoubleBuffered = True
+        Me.Name = "Form1"
+        Me.Text = "Protect Tiki Land"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ArizonWall, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Bullet, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents ArizonWall As PictureBox
+    Friend WithEvents Bullet As PictureBox
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Label1 As Label
+End Class
---
+++ GameV1/Form1.vb
@@ -0,0 +1,128 @@
+﻿Public Class Form1
+    Dim R As New Random
+    Dim score As Integer
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        Select Case e.KeyCode
+
+            Case Keys.Up
+                MoveTo(PictureBox1, 0, -15)
+            Case Keys.Down
+                MoveTo(PictureBox1, 0, 15)
+            Case Keys.Left
+                MoveTo(PictureBox1, -15, 0)
+            Case Keys.Right
+                MoveTo(PictureBox1, 15, 0)
+            Case Keys.Space
+                CreateNew("bullet", Bullet, PictureBox1.Location)
+            Case Keys.R
+                PictureBox1.Image.RotateFlip(RotateFlipType.Rotate90FlipX)
+                Me.Refresh()
+
+        End Select
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+    End Sub
+
+    Sub follow(p As PictureBox)
+        Static headstart As Integer
+        Static c As New Collection
+        c.Add(PictureBox1.Location)
+        headstart = headstart + 1
+        If headstart > 10 Then
+            p.Location = c.Item(1)
+            c.Remove(1)
+        End If
+    End Sub
+
+    Public Sub chase(p As PictureBox)
+        Dim x, y As Integer
+        If p.Location.X > PictureBox1.Location.X Then
+            x = -5
+        Else
+            x = 5
+        End If
+        MoveTo(p, x, 0)
+        If p.Location.Y < PictureBox1.Location.Y Then
+            y = 5
+        Else
+            y = -5
+        End If
+        MoveTo(p, x, y)
+    End Sub
+
+    Function Collision(p As PictureBox, t As String)
+        Dim col As Boolean
+
+        For Each c In Controls
+            Dim obj As Control
+            obj = c
+            If p.Bounds.IntersectsWith(obj.Bounds) And obj.Name.ToUpper.Contains(t.ToUpper) Then
+                col = True
+            End If
+        Next
+        Return col
+    End Function
+    'Return true or false if moving to the new location is clear of objects ending with t
+    Function IsClear(p As PictureBox, distx As Integer, disty As Integer, t As String) As Boolean
+        Dim b As Boolean
+
+        p.Location += New Point(distx, disty)
+        b = Not Collision(p, t)
+        p.Location -= New Point(distx, disty)
+        Return b
+        Dim f As New Form2
+        f.ShowDialog()
+        Me.Visible = True
+    End Function
+    'Moves and object (won't move onto objects containing  "wall" and shows green if object ends with "win"
+    Sub MoveTo(p As PictureBox, distx As Integer, disty As Integer)
+        If IsClear(p, distx, disty, "WALL") Then
+            p.Location += New Point(distx, disty)
+        End If
+
+        If p.Name = "PictureBox1" And Collision(p, "WIN") Then
+            Me.BackColor = Color.Green
+            Return
+        End If
+    End Sub
+
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
+
+    End Sub
+    Dim bDir As Integer = 5
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        If Bullet.Location.X < 0 Then
+            bDir = 10
+        End If
+        If Bullet.Location.X > 1000 Then
+            bDir = -10
+        End If
+        MoveTo(Bullet, bDir, 0)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Sub CreateNew(name As String, pic As PictureBox, location As Point)
+        Dim p As New PictureBox
+        p.Location = location
+        p.Image = pic.Image
+        p.BackColor = pic.BackColor
+        p.Name = name
+        p.Width = pic.Width
+        p.Height = pic.Height
+        p.SizeMode = PictureBoxSizeMode.StretchImage
+        Controls.Add(p)
+
+    End Sub
+
+    Sub Randmove(P As PictureBox)
+        Dim X As Integer
+        Dim Y As Integer
+        X = R.Next(-10, 11)
+        Y = R.Next(-10, 11)
+        MoveTo(P, X, Y)
+    End Sub
+End Class
+
